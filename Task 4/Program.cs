a229aced1d1886f89dc08ab09eb041e2c37e551d
Stanislav Tkachenko <infastiko@gmail.com>
2022-11-15 16:22:13 +0400

    added Task 4

---
+++ Task 4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int maxnum = num1;
+
+if (num2 > num1)
+{
+    maxnum = num2;
+}
+if (num3 > num2)
+{
+    maxnum = num3;
+}
+Console.WriteLine($"Максимальное число - {maxnum}");
