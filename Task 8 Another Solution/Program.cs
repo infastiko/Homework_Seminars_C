0f52771d92c02365447b5874f3d398533f70c2c3
Stanislav Tkachenko <infastiko@gmail.com>
2022-11-15 16:44:03 +0400

    added Task 8 Another Solution

---
+++ Task 8 Another Solution/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while (count <= num)
+{
+    if ((count % 2) == 0)
+    {
+        Console.WriteLine(count);
+        count += 1;
+    }
+    else
+    {
+        count += 1;
+    }
+}
