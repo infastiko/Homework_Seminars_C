6684d4371fa24a8b4f22c43f0fd1cef4af2adb43
Stanislav Tkachenko <infastiko@gmail.com>
2022-11-15 16:27:07 +0400

    added Task 6

---
+++ Task 6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("Это четное число.");
+}
+else
+{
+    Console.WriteLine("Это нечетное число.");
+}
