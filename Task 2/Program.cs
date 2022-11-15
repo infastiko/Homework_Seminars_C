f6cf3f253f5eca2bf60386a0da445ad31df1b5a1
Stanislav Tkachenko <infastiko@gmail.com>
2022-11-15 16:16:13 +0400

    added Task 2

---
+++ Task 2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine($"{number1} - большее, {number2} - меньшее.");
+}
+else if (number1 < number2)
+{
+    Console.WriteLine($"{number2} - большее, {number1} - меньшее.");
+}
+else
+{
+    Console.WriteLine("Вы ввели два равных числа.");
+}
