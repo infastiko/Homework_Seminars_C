7fec09923ecb0c87780e8aae19d201816ebbfef6
Stanislav Tkachenko <infastiko@gmail.com>
2022-11-17 20:58:30 +0400

    added Task 13

---
+++ Task 13/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 100 && num > -100)
+{
+    Console.WriteLine($"В числе {num} нет третьей цифры.");
+}
+
+if (num < 0)
+{
+    num = -num;
+}
+
+while (num > 999)
+{
+    num = (num / 10);
+}
+    
+if (num > 99 && num < 1000)
+{
+    Console.WriteLine($"Третья цифра заданного числа: {num % 10}"); 
+}
