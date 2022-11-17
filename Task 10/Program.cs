142169c44039c63f9f584f5a69843a1625c14791
Stanislav Tkachenko <infastiko@gmail.com>
2022-11-17 20:56:30 +0400

    added Task 10

---
+++ Task 10/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 0)
+{
+    num = -num;
+}
+
+if (num < 100 || num > 999)
+{
+    Console.WriteLine("Вы ввели не трехзначное число.");
+}
+else
+{
+Console.WriteLine($"Вторая цифра данного числа: {((num / 10) % 10)}");
+}
