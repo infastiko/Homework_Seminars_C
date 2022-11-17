e78e5cffe5e00ac400b0f065c5bec68d7c4a0e4a
Stanislav Tkachenko <infastiko@gmail.com>
2022-11-17 21:00:15 +0400

    added Task 13 2nd Solution

---
+++ Task 13 2nd Solution/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+string num = Console.ReadLine();
+int Length = num.Length;
+if (num[0] == '-' && Length < 4 || Length < 3) 
+{
+    Console.WriteLine("В этом числе нет третьей цифры.");
+}
+else if (num[0] == '-')
+{
+    Console.WriteLine($"Третья цифра заданного числа: {num[3]}");
+}
+else if (num[0] != '-')
+{
+    Console.WriteLine($"Третья цифра заданного числа: {num[2]}");
+}
