08710569a3f2d68ae10b2adfadcd6190f1db4625
Stanislav Tkachenko <infastiko@gmail.com>
2022-11-20 14:29:59 +0400

    added Task 19 2nd Solution

---
+++ Task 19 2nd Solution/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Write("Введите пятизначное или шестизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 9999 && num < 100000)
+{
+    if  ((num / 10000 == num % 10) && (((num / 1000) % 10) == (num / 10) % 10))
+    {
+        Console.WriteLine($"Число {num} является палиндромом.");
+    }
+    else
+    {
+        Console.WriteLine($"Число {num} НЕ является палиндромом.");
+    }
+
+}
+
+else if (num > 99999 && num < 1000000)
+{
+    if  ((num / 100000 == num % 10) && ((num / 10000) % 10) == ((num / 10) % 10) && ((num / 1000) % 10) == ((num / 100) % 10))
+    {
+        Console.WriteLine($"Число {num} является палиндромом.");
+    }
+    else
+    {
+        Console.WriteLine($"Число {num} НЕ является палиндромом.");
+    }
+
+}
+
+else
+{
+    Console.WriteLine("Вы ввели число, не входящее в условленные диапазоны.");
+}
