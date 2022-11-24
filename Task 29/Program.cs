feb762f6d4d7547b82bff9c3fbede04a1e8e9767
Stanislav Tkachenko <infastiko@gmail.com>
2022-11-24 22:40:22 +0400

    added Task 29

---
+++ Task 29/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Write("Введите желаемую длину массива: ");
+int mas = Convert.ToInt32(Console.ReadLine());
+int [] arr = new int [mas];
+for(int i = 0; i < arr.Length; i ++)
+{
+    Console.Write("Введите число для массива: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    arr[i] = num;
+}
+Console.WriteLine('[' + string.Join(", ", arr) + ']');
