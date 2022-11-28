7cd71f1b6ad09fd077cb777281b5056fcf9dfbee
Stanislav Tkachenko <infastiko@gmail.com>
2022-11-28 19:10:52 +0400

    added Task 38

---
+++ Task 38/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 38: Задайте массив вещественных(тип double) чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+double[] FillArray(int size)
+{
+    double[] arr = new double[size];
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Math.Round((Convert.ToDouble(new Random().Next(0, 100)) + new Random().NextDouble()),2);
+    }
+    return arr;
+}
+Console.Write("Введите длину массива: ");
+int num = Convert.ToInt32(Console.ReadLine());
+double[] array = FillArray(num);
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+double max = array[0];
+double min = array[0];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i] > max)
+    {
+        max = array[i];
+    }
+    if(array[i] < min)
+    {
+        min = array[i];
+    }
+}
+Console.WriteLine($"Максимальный элемент массива: {max}");
+Console.WriteLine($"Минимальный элемент массива: {min}");
+Console.WriteLine($"Разница элементов максимального и минимального элемента массива равна {max - min}");
