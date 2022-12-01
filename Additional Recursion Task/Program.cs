aeeafb2dc8d6540c7a1266f489dcf9b412b10b13
Stanislav Tkachenko <infastiko@gmail.com>
2022-12-01 17:31:04 +0400

    added Additional Recursion Task

---
+++ Additional Recursion Task/Program.cs
@@ -0,0 +1,19 @@
+﻿// Дополнительная задача на рекурсию (необязательная)
+// Напишите программу для подсчета количества цифр в числе с помощью рекурсии.
+// 12345 -> 5
+
+void countNumber(int num, int count)
+{
+    if (num == 0)
+    {
+        Console.WriteLine($"Количество цифр в заданном числе: {count}");
+        return;
+    }
+    countNumber(num / 10, count = count + 1);
+}
+
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+countNumber(num, count);
