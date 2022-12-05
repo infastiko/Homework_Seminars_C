9ebed9fbc970ed1a20850ee6c77ab02d44ed13d5
Stanislav Tkachenko <infastiko@gmail.com>
2022-12-05 21:23:31 +0400

    added Task 47

---
+++ Task 47/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+// 1 -3,3 8 -9,9
+
+// 8 7,8 -7,1 9
+
+Console.WriteLine("Введите размерность двумерного массива m x n");
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double [,] matrix = new double [m,n];
+for(int i = 0; i < m; i++)
+{
+    for(int j = 0; j < n; j++)
+    {
+        matrix[i, j] = Math.Round((Convert.ToDouble(new Random().Next(-10, 11)) + new Random().NextDouble()),1);
+        Console.Write(String.Format("{0,7}", matrix[i, j]));
+    }
+    Console.WriteLine();
+}
